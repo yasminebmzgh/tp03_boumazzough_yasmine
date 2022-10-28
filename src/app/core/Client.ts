ae4efcbbc6be108163c718e10f5224e4ea2ca526
Yasmine Boumazzough <yasminebmzgh@macbook-air-de-yasmine.home>
2022-10-28 23:14:15 +0200

    final

--- src/app/core/Client.ts
+++ src/app/core/Client.ts
@@ -1,27 +1,33 @@
-export class Client{
-    civilite: string="";
-    nom: string="";
-    prenom: string="";
-    telephone: string="";
-    email: string="";
-    adresse: string="";
-    ville: string="";
-    code_postal: string="";
-    login: string="";
-    password: string="";
-    password_conf:string="";
-      
-    isEmpty (){
-      return (this.civilite.length===0)&&
-      (this.nom.length===0)&&
-      (this.prenom.length===0)&&
-      (this.telephone.length===0)&&
-      (this.email.length===0)&&
-      (this.adresse.length===0)&&
-      (this.ville.length===0)&&
-      (this.code_postal.length===0)&&
-      (this.login.length===0)&&
-      (this.password.length===0)&&
-      (this.password_conf.length===0) as boolean
-    }
-  }
+export class Client {
+	nom : string;
+	prenom : string;
+	adress : string;
+	cp : "";
+	ville : string;
+	tel : string;
+	email : string;
+	civilite : string;
+	mdp : string= '';
+	confirmMdp: String = '';
+	login : string;
+	pays : string;
+	
+
+	
+
+	constructor () {
+		this.nom = "";
+		this.prenom = "";
+		//this.age = 0;
+		this.adress ="";
+		this.cp="";
+		this.ville="";
+		this.tel="";
+		this.email="";
+		this.civilite="";
+		this.mdp="";
+		this.login="";
+		this.pays="";
+	}
+
+}
